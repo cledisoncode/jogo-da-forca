3462ed121182438b5a62b81d4dedb35725efb14b
cledisoncode <200339853+cledisoncode@users.noreply.github.com>
2025-05-28 14:26:23 -0300

    acrescentando mais palavra

--- src/js/palavras.js
+++ src/js/palavras.js
@@ -1,26 +1,26 @@
 const palavras = [
     palavra001 = {
-        nome: "JUDE BLACK STACY",
+        nome: "JUDE BLACK",
         categoria: "HYGTG PERSONAGEM"
     },
     palavra002 = {
-        nome: "GWENDOLYN BLACK STACY",
+        nome: "GWENDOLYN STACY",
         categoria: "HYGTG PERSONAGEM"
     },
     palavra003 = {
-        nome: "JESS BISHOP",
+        nome: "JESS BISHOP MAXIMOFF",
         categoria: "CONTROL PERSONAGEM"
     },
     palavra004 = {
-        nome: "KATE MAXIMORF",
+        nome: "KATE BISHOP MAXIMOFF",
         categoria: "CONTROL PERSONAGEM"
     },
     palavra005 = {
-        nome: "YELENA",
+        nome: "YELENA BELOVA",
         categoria: "CONTROL BEST FRIEND"
     },
     palavra006 = {
-        nome: "PETER",
+        nome: "PETER PARKER",
         categoria: "HYGTG BEST FRIEND"
     },
     palavra007 = {
@@ -40,11 +40,85 @@ const palavras = [
         categoria: "CONTROL FILME"
     },
     palavra011 = {
-        nome: "NATASHA",
+        nome: "NATASHA ROMANOFF",
         categoria: "CONTROL MAE"
     },
     palavra012 = {
         nome: "MARY JANE",
         categoria: "HYGTG TALARICO"
-    }
+    },
+    palavra013 = {
+        nome: "MILK SHAKE DE MORANGO",
+        categoria: "HYGTG BEBIBA"
+    },
+
+    palavra014 = {
+        nome: "STARBUCKS",
+        categoria: "CONTROL BEBIBA"
+    },
+    palavra015 = {
+        nome: "ELLIE BLACK",
+        categoria: "HYGTG PERSONAGEM"
+    },
+    palavra016 = {
+        nome: "MARY",
+        categoria: "CONTROL MAE"
+    },
+    palavra017 = {
+        nome: "MJ",
+        categoria: "HYGTG PERSONAGEM"
+    },
+    palavra018 = {
+        nome: "AUGUSTUS BLACK",
+        categoria: "HYGTG PERSONAGEM CUZAO"
+    },
+    palavra019 = {
+        nome: "SABRINA BLACK",
+        categoria: "HYGTG PERSONAGEM CUZAO"
+    },
+    palavra020 = {
+        nome: "ALICE COOPER",
+        categoria: "HYGTG PERSONAGEM "
+    },
+    palavra021 = {
+        nome: "NICOLE WILLOW",
+        categoria: "HYGTG PERSONAGEM INSIGNIFICANTE"
+    },
+    palavra022 = {
+        nome: "NED LEEDS",
+        categoria: "HYGTG PERSONAGEM INSIGNIFICANTE"
+    },
+    palavra023 = {
+        nome: "HARRY OSBORN",
+        categoria: "HYGTG PERSONAGEM CUZAO"
+    },
+    palavra025 = {
+        nome: "WANDA MAXIMOFF",
+        categoria: "CONTROL VILAO"
+    },
+    palavra026 = {
+        nome: "KYRA GALKIN",
+        categoria: "CONTROL PERSONAGEM"
+    },
+    palavra027 = {
+        nome: "STEFAN LAURENCE",
+        categoria: "CONTROL PERSONAGEM"
+    },
+    palavra028 = {
+        nome: "DOUTOR ESTRANHO",
+        categoria: "CONTROL PERSONAGEM"
+    },
+    palavra029 = {
+        nome: "AMERICA CHAVEZ",
+        categoria: "CONTROL PERSONAGEM"
+    },
+
+    palavra030 = {
+        nome: "DON T BLAME ME - TAYLOR SWIFT",
+        categoria: "CONTROL MUSICA"
+    },
+
+
+    
+
 ];
